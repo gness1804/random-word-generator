16593ec4f596454170543415e0ab5b5e7e5bd510
gness1804 <graham.nessler@gmail.com>
2021-02-12 17:39:15 -0600

    Got random word showing up on screen

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-import express, { Request, Response } from 'express';
+import express = require('express');
+import fs = require('fs');
+import path = require('path');
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: express.Request, res: express.Response) => {
+  const file = path.join(__dirname, '../src/words.json');
+  const words = await fs.promises.readFile(file, 'utf-8');
+  const parsedWords: string[] = JSON.parse(words);
+  const randomIndex = Math.floor(Math.random() * (parsedWords.length - 0 + 1) + 0);
+  const word = parsedWords[randomIndex];
+
   res.header({
     'Access-Control-Allow-Origin': '*',
   });
-  res.json({ message: 'Hello world' });
+  res.json({ word });
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   /* eslint-disable-next-line no-console */
   console.info('Server running on port 8080.');
 });
-
-
